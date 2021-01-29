ac53bcfaa8a4d5c53e7365fcda359c233bac9f46
Alex Soto <asotobu@gmail.com>
2021-01-29 11:38:31 +0100

    Update version

--- src/test/java/org/acme/OpenIdAuthenticationTestBeforeClassCallback.java
+++ src/test/java/org/acme/OpenIdAuthenticationTestBeforeClassCallback.java
@@ -1,9 +1,5 @@
 package org.acme;
 
-import java.lang.annotation.Annotation;
-import java.util.Arrays;
-import java.util.Optional;
-
 import org.acme.api.OpenIdAuthentication;
 
 import io.quarkus.test.junit.callback.QuarkusTestBeforeClassCallback;
@@ -16,24 +12,17 @@ public class OpenIdAuthenticationTestBeforeClassCallback implements QuarkusTestB
     public void beforeClass(Class<?> testClass) {
         
 
-        /**System.out.println("***");
-        Annotation[] annotations = testClass.getAnnotations();
+        OpenIdAuthentication openIdAuth = testClass.getAnnotation(OpenIdAuthentication.class);
 
-        Optional<Annotation> annotation = Arrays.stream(annotations)
-            .filter(a -> a.annotationType().getSimpleName().equals(OpenIdAuthentication.class.getSimpleName()))
-            .findFirst();
+        if (openIdAuth != null) {
 
-        annotation.ifPresent(a -> {
-            OpenIdAuthentication openIdAuth =  (OpenIdAuthentication) a;
-
-            System.out.println("OpenId Connect authentication logic with " + openIdAuth.username() + " " + openIdAuth.password());
             String accessToken = getToken(openIdAuth);
 
             final RequestSpecBuilder requestSpec = new RequestSpecBuilder();
             requestSpec.addHeader("Authorization", "Bearer " + accessToken);
             
             RestAssured.requestSpecification = requestSpec.build();
-        });**/
+        }
         
     }
 
